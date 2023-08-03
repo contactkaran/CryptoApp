9ced10dd1ebd2445caadcc8b90b1ad5a023d26bc
contactkaran <ksmakan@gmail.com>
2023-08-03 14:35:41 -0400

    Colors added

---
+++ app/src/main/java/com/contactkaran/cryptoapp/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.contactkaran.cryptoapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black200 = Color(0xFF3f3e46)
+val Black500 = Color(0xFF252429)
+
+val Red = Color(0xffeb6564)
+val Green = Color(0xffc5ec6b)
