b80eedc344c3100e95f92be7dcfa95e238fe021a
contactkaran <ksmakan@gmail.com>
2023-08-03 14:40:16 -0400

    Themes.kt updated with colors and SystemUIController - statusBar invisible

--- app/src/main/java/com/contactkaran/cryptoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/contactkaran/cryptoapp/ui/theme/Theme.kt
@@ -14,17 +14,24 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+
+    primary = Black200,
+    secondary = Black500
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+//    primary = Purple40,
+//    secondary = PurpleGrey40,
+//    tertiary = Pink40
+
+    primary = Black200,
+    secondary = Black500
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -61,6 +68,8 @@ fun CryptoAppTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
+
+    rememberSystemUiController().isStatusBarVisible = false
 
     MaterialTheme(
         colorScheme = colorScheme,
